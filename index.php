2b6e5e23ecb5b404a134f1006322306b96592639
Chico-Navez <ngiannulli@gmail.com>
2017-09-22 13:05:21 -0700

    Testing

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
               <?php
               session_start();//start your session
               include('facebook-login/facebook_info.php');//make sure to include your facebook credentials!
-              include('facebook-login/libraries/php-graph-sdk/src/Facebook');//then you'll need to include the facebook sdk
+              include('facebook-login/libraries/php-graph-sdk/src/Facebook/autoload.php');//then you'll need to include the facebook sdk
 
               $fb = new Facebook\Facebook([//create a new facebook object
                 'app_id' => FACEBOOK_APP_ID, //Replace {app-id} with your app id
