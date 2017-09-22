918853be2b5ee72bd6d95c39d7fdf0937f0fbf2d
calebeich7 <dalethreesixty@outlook.com>
2017-09-22 10:29:31 -0700

    setup for conversion

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        './index.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    context: resolve(__dirname, 'src'),
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js|.jsx?$/,
+                use: [ 'babel-loader' ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [ 'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack-loader?bypassOnDebug' ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin()
+    ]
+};
