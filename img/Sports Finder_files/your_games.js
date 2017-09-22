e983d67bb0d838774d495d309de602e2e0c6a1d1
cbryant24 <chrs.bryant@gmail.com>
2017-09-22 15:17:12 -0700

    working on cancel game post

--- img/Sports Finder_files/your_games.js
+++ img/Sports Finder_files/your_games.js
@@ -8,17 +8,17 @@ $('document').ready(() => {
     //     }
 
     // })
-    display_games.load_games();
-    view_game = new View_Game();
+    your_games.load_games();
+    cancel_game = new Cancel_Game;
     $('.viewbtn').on('click', function(event){
         event.preventDefault();
-        view_game.load_display(event)
+        cancel_game.load_display(event)
     })
 })
 
 function myMap() {
     //creation of the new Find Game object
-    display_games = new Display_Games();
+    your_games = new Your_Games();
 
     var mapProp= {
         center:new google.maps.LatLng(33.552085,-117.666035),
@@ -32,21 +32,21 @@ function myMap() {
     var infowindow = new google.maps.InfoWindow();
     
     var marker, i;
-    for (i = 0; i < display_games.all_games.length; i++) {  
+    for (i = 0; i < your_games.all_games.length; i++) {  
         marker = new google.maps.Marker({
-        position: new google.maps.LatLng(display_games.all_games[i][1], display_games.all_games[i][2]),
+        position: new google.maps.LatLng(your_games.all_games[i][1], your_games.all_games[i][2]),
         map: map
         });
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-              infowindow.setContent(display_games.all_games[i][0]);
+              infowindow.setContent(your_games.all_games[i][0]);
               infowindow.open(map, marker);
             }
           })(marker, i));
         }
 }
 
-function Display_Games(games) {
+function Your_Games() {
     this.complete_games = [{
         game_title: ' Ballers ',
         description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid beatae dolorum, ducimus eaque in nemo quo suscipit? A et impedit iusto quidem sit! Aut ea nihil optio quae! Deleniti dolorem id maiores minima nostrum obcaecati reiciendis, rem ullam velit vero. Accusamus aliquid architecto impedit laboriosam, quas saepe vero. Animi beatae doloremque doloribus exercitationem ipsa molestias quae tempore tenetur totam voluptates? Atque aut doloremque minus modi nihil nisi numquam ullam vitae. A aliquid corporis doloribus ea eos eveniet explicabo harum maxime modi non, officiis omnis pariatur placeat quaerat quam quasi, quo rerum sed sunt tempora! Amet iusto pariatur quos voluptatem. ',
@@ -57,7 +57,6 @@ function Display_Games(games) {
        game_time: ' 7:30 ',
        game_date: '11/12/2017',
        vibe: ' casual ',
-       game_id: 0
    },
    {
        game_title: ' Brick City ',
@@ -74,7 +73,6 @@ function Display_Games(games) {
        game_time: ' 9:00 ',
        game_date: '11/12/2017',
        vibe: 'casual ',
-       game_id: 0
    },
    {
        game_title: ' Buckets ',
@@ -92,7 +90,6 @@ function Display_Games(games) {
        game_time: ' 10:00 ',
        game_date: '11/12/2017',
        vibe: 'casual ',
-       game_id: 0
    },
    {
        game_title: ' Ankle Breakers ',
@@ -110,7 +107,6 @@ function Display_Games(games) {
        game_time: ' 12:00 ',
        game_date: '11/12/2017',
        vibe: ' casual ',
-       game_id: 0
    },
    {
        game_title: 'Bitches and Ballers ',
@@ -128,15 +124,11 @@ function Display_Games(games) {
        game_time: ' 18:00 ',
        game_date: '11/12/2017',
        vibe: ' competitive ',
-       game_id: 0
    }];
     this.all_games = [
         ['Bondi Beach', 33.552085, -117.666035, 4],
-        ['Coogee Beach', 33.697192, -117.799778, 5],
-        ['Cronulla Beach', 33.824524, -117.837415, 3],
-        ['Manly Beach', 33.542719, -117.785357, 2],
-        ['Maroubra Beach', 33.427352, -117.612600, 1]
     ];
+
     this.load_games = function(){
         let $game_list_container = $('.game-list-container');
         let $game_container = $('<div>')
@@ -162,15 +154,12 @@ function Display_Games(games) {
             })
             let $view_button = $('<a>', {
                 href: '',
-                id: this.game_id,
                 class: 'btn btn-outline btn-xl viewbtn',
-                text: 'View',
-                game_info: `${this.complete_games[i].game_title}|${this.complete_games[i].description}|${this.complete_games[i].game_date}|${this.complete_games[i].game_time}`
-                
+                text: 'Cancel?',
+                game_id: i,
+                random: `${this.complete_games[i].game_title}|${this.complete_games[i].description}|${this.complete_games[i].game_date}|${this.complete_games[i].game_time}`
             })
-            let view_info = $view_button[0];
-            view_info.game = this.complete_games[i];
-            // $('.viewbtn').on('click', join_game.display_selected('value'))
+            // $('.viewbtn').on('click', cancel_game.display_selected('value'))
             $single_game.append($game_title);
             $single_game.append($game_time);
             $single_game.append($game_vibe);
@@ -182,28 +171,11 @@ function Display_Games(games) {
 }
 
 //Need to check if they already have an active game on there profile
-function Join_Game() {
-    this.complete_game = {
-        game_title: '',
-        game_time: null,
-        game_address: '',
-        lat_lon: {
-            lat: '',
-            lon: ''
-        },
-        game_date: '',
-        game_vibe: '',
-        game_description: '',
-        game_creator: false
-    }
-}
-
-function View_Game() {
+function Cancel_Game() {
     this.load_display = function(game_details) {
-        $('.gameinfobox').empty()
         debugger
         let $gameinfobox = $('.gameinfobox')
-        let game_arr = game_details.target.attributes[2].textContent.split('|');
+        let game_arr = game_details.target.attributes[3].textContent.split('|');
         let $game_title = $('<h3>', {
             text: game_arr[0]
         })
@@ -218,7 +190,7 @@ function View_Game() {
         let $cancel_btn = $('<a>', {
             href: '#',
             class: 'btn btn-outline btn-xl joinbtn',
-            text: 'Join Game'
+            text: 'Cancel'
         })
         $gameinfobox.append($game_title);
         $gameinfobox.append('<hr>');
@@ -226,9 +198,15 @@ function View_Game() {
         $gameinfobox.append($game_date);
         $gameinfobox.append('<hr>');
         $gameinfobox.append($cancel_btn);
+        
+    }
+    this.load_click = function() {
+        
+    }
+    this.cancel_click = function() {
+
     }
 }
 
-var display_games = null;
-var join_game = null;
-var view_game = null;
+var your_games = null;
+var cancel_game = null;
--- your_games.js
+++ your_games.js
@@ -1,24 +1,28 @@
 $('document').ready(() => {
     // $.ajax({
     //     url: 'php.data',
-    //     type: 'post',
+    //     type: 'get',
     //     dataType: 'json',
     //     success(data){
     //         display_games.load_games(data)
     //     }
 
     // })
-    display_games.load_games();
-    view_game = new View_Game();
-    $('.viewbtn').on('click', function(event){
-        event.preventDefault();
-        view_game.load_display(event)
-    })
+    your_games.load_games();
+        // $('.viewbtn').on('click', function(){
+        //     cancel_game.load_display(this)
+        // })
 })
 
 function myMap() {
     //creation of the new Find Game object
-    display_games = new Display_Games();
+    cancel_game = new Cancel_Game
+    your_games = new Your_Games();
+    $('.joinbtn').on('click', function(event){
+        event.preventDefault()
+        debugger
+        cancel_game.load_display(this)
+    })
 
     var mapProp= {
         center:new google.maps.LatLng(33.552085,-117.666035),
@@ -32,21 +36,21 @@ function myMap() {
     var infowindow = new google.maps.InfoWindow();
     
     var marker, i;
-    for (i = 0; i < display_games.all_games.length; i++) {  
+    for (i = 0; i < your_games.all_games.length; i++) {  
         marker = new google.maps.Marker({
-        position: new google.maps.LatLng(display_games.all_games[i][1], display_games.all_games[i][2]),
+        position: new google.maps.LatLng(your_games.all_games[i][1], your_games.all_games[i][2]),
         map: map
         });
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-              infowindow.setContent(display_games.all_games[i][0]);
+              infowindow.setContent(your_games.all_games[i][0]);
               infowindow.open(map, marker);
             }
           })(marker, i));
         }
 }
 
-function Display_Games(games) {
+function Your_Games() {
     this.complete_games = [{
         game_title: ' Ballers ',
         description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid beatae dolorum, ducimus eaque in nemo quo suscipit? A et impedit iusto quidem sit! Aut ea nihil optio quae! Deleniti dolorem id maiores minima nostrum obcaecati reiciendis, rem ullam velit vero. Accusamus aliquid architecto impedit laboriosam, quas saepe vero. Animi beatae doloremque doloribus exercitationem ipsa molestias quae tempore tenetur totam voluptates? Atque aut doloremque minus modi nihil nisi numquam ullam vitae. A aliquid corporis doloribus ea eos eveniet explicabo harum maxime modi non, officiis omnis pariatur placeat quaerat quam quasi, quo rerum sed sunt tempora! Amet iusto pariatur quos voluptatem. ',
@@ -57,7 +61,7 @@ function Display_Games(games) {
        game_time: ' 7:30 ',
        game_date: '11/12/2017',
        vibe: ' casual ',
-       game_id: 0
+       game_id: 5
    },
    {
        game_title: ' Brick City ',
@@ -74,7 +78,7 @@ function Display_Games(games) {
        game_time: ' 9:00 ',
        game_date: '11/12/2017',
        vibe: 'casual ',
-       game_id: 0
+       game_id: 4
    },
    {
        game_title: ' Buckets ',
@@ -92,7 +96,7 @@ function Display_Games(games) {
        game_time: ' 10:00 ',
        game_date: '11/12/2017',
        vibe: 'casual ',
-       game_id: 0
+       game_id: 3
    },
    {
        game_title: ' Ankle Breakers ',
@@ -110,7 +114,7 @@ function Display_Games(games) {
        game_time: ' 12:00 ',
        game_date: '11/12/2017',
        vibe: ' casual ',
-       game_id: 0
+       game_id: 2
    },
    {
        game_title: 'Bitches and Ballers ',
@@ -128,16 +132,16 @@ function Display_Games(games) {
        game_time: ' 18:00 ',
        game_date: '11/12/2017',
        vibe: ' competitive ',
-       game_id: 0
+       game_id: 1
    }];
     this.all_games = [
         ['Bondi Beach', 33.552085, -117.666035, 4],
-        ['Coogee Beach', 33.697192, -117.799778, 5],
-        ['Cronulla Beach', 33.824524, -117.837415, 3],
-        ['Manly Beach', 33.542719, -117.785357, 2],
-        ['Maroubra Beach', 33.427352, -117.612600, 1]
     ];
+
     this.load_games = function(){
+        this.complete_games.sort( (a,b) => {
+            return a.game_id - b.game_id
+        } )
         let $game_list_container = $('.game-list-container');
         let $game_container = $('<div>')
         for(let i = 0; i < this.complete_games.length; i++) {
@@ -162,73 +166,47 @@ function Display_Games(games) {
             })
             let $view_button = $('<a>', {
                 href: '',
-                id: this.game_id,
                 class: 'btn btn-outline btn-xl viewbtn',
-                text: 'View',
-                game_info: `${this.complete_games[i].game_title}|${this.complete_games[i].description}|${this.complete_games[i].game_date}|${this.complete_games[i].game_time}`
-                
+                text: 'Cancel?',
             })
-            let view_info = $view_button[0];
-            view_info.game = this.complete_games[i];
-            // $('.viewbtn').on('click', join_game.display_selected('value'))
-            $single_game.append($game_title);
-            $single_game.append($game_time);
-            $single_game.append($game_vibe);
+            let view_button = $view_button[0];
+            view_button.game = this.complete_games[i];
+            $single_game.append($game_title, $game_time, $game_vibe);
             $view_div.append($view_button);
             $single_game.append($view_div);
             $game_list_container.append($single_game);
         }
+        cancel_game.load_display(this.complete_games[0].game_id)
     }
 }
 
 //Need to check if they already have an active game on there profile
-function Join_Game() {
-    this.complete_game = {
-        game_title: '',
-        game_time: null,
-        game_address: '',
-        lat_lon: {
-            lat: '',
-            lon: ''
-        },
-        game_date: '',
-        game_vibe: '',
-        game_description: '',
-        game_creator: false
-    }
-}
-
-function View_Game() {
-    this.load_display = function(game_details) {
+function Cancel_Game() {
+    this.load_display = function(game_selected_id) {        
         $('.gameinfobox').empty()
-        debugger
         let $gameinfobox = $('.gameinfobox')
-        let game_arr = game_details.target.attributes[2].textContent.split('|');
         let $game_title = $('<h3>', {
-            text: game_arr[0]
+            text: your_games.complete_games[game_selected_id].game_title
         })
         let $game_descr = $('<p>',{
-            text: game_arr[1],
+            text: your_games.complete_games[game_selected_id].description,
         })
-
         let $game_date = $('<h6>', {
-            text: `${game_arr[2]} ${game_arr[3]}`
+            text: `${your_games.complete_games[game_selected_id].game_date} ${your_games.complete_games[game_selected_id].game_time}`
         })
-
         let $cancel_btn = $('<a>', {
-            href: '#',
+            href: '',
             class: 'btn btn-outline btn-xl joinbtn',
-            text: 'Join Game'
+            text: 'Cancel'
         })
-        $gameinfobox.append($game_title);
-        $gameinfobox.append('<hr>');
-        $gameinfobox.append($game_descr);
-        $gameinfobox.append($game_date);
-        $gameinfobox.append('<hr>');
-        $gameinfobox.append($cancel_btn);
+        $gameinfobox.append($game_title, '<hr>', $game_descr, $game_date, '<hr>', $cancel_btn);
+
+    }
+    this.cancel_click = function() {
+
+
     }
 }
 
-var display_games = null;
-var join_game = null;
-var view_game = null;
+var your_games = null;
+var cancel_game = null;
