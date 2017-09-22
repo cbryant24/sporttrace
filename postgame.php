593dd6adbaa9d2f60acc23b3fb43d3311bb38cb1
Chico-Navez <ngiannulli@gmail.com>
2017-09-22 15:44:43 -0700

    more php changes

--- postgame.php
+++ postgame.php
@@ -26,7 +26,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a href="index.html">
+        <a href="index.php">
           <img src="img/logo2.png" width="190">
         </a>
         <div class="nav-link-box">
