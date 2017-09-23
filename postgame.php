a668caec6aac753656bd7e6e971e7eded2da97b3
Chico-Navez <ngiannulli@gmail.com>
2017-09-22 17:36:04 -0700

    FUxInG wItH PHP

--- postgame.php
+++ postgame.php
@@ -1,3 +1,7 @@
+<?php 
+  session_start();//start your session
+  include('facebook-login/facebook_info.php');//make sure to include your facebook credentials!
+  include('facebook-login/libraries/php-graph-sdk/src/Facebook/autoload.php');//then you'll need to include the facebook sdk ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,9 +43,7 @@
             </li>
             <li class="nav-item">
               <?php
-              session_start();//start your session
-              include('facebook-login/facebook_info.php');//make sure to include your facebook credentials!
-              include('facebook-login/libraries/php-graph-sdk/src/Facebook/autoload.php');//then you'll need to include the facebook sdk
+
 
               $fb = new Facebook\Facebook([//create a new facebook object
                 'app_id' => FACEBOOK_APP_ID, //Replace {app-id} with your app id
