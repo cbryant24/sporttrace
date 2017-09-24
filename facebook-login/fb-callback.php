df9cb9169d8a95bebcbca68664a9c6be26be9972
Chico-Navez <ngiannulli@gmail.com>
2017-09-23 20:15:37 -0700

    php

--- facebook-login/fb-callback.php
+++ facebook-login/fb-callback.php
@@ -75,6 +75,24 @@ if (! $accessToken->isLongLived()) {
 
 $_SESSION['fb_access_token'] = (string) $accessToken;
 
+
+try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->get('me?fields=id,name,email', "{$_SESSION['fb_access_token']}");
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$user = $response->getGraphUser();
+
+
+  $_SESSION['user_name'] = $user['name'];
+  $_SESSION['user_id'] = $user['id'];
+  $_SESSION['user_email'] = $user['email'];
 // User is logged in with a long-lived access token.
 // You can redirect them to a members-only page.
 header('Location: http://tittyking.com/sportsfinder/findgame.php');
