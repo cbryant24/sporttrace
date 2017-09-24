f4608d62557f89177fa6739a1652942beecc5576
Chico-Navez <ngiannulli@gmail.com>
2017-09-23 23:01:18 -0700

    testing

--- facebook-login/logout.php
+++ facebook-login/logout.php
@@ -4,10 +4,10 @@
 	if(isset($_SESSION['fb_access_token'])){
 		$logouturl = 'https://www.facebook.com/logout.php?next='.'http://tittyking.com/sportsfinder/'.'&access_token='.$_SESSION['fb_access_token'];
 
-		unset $_SESSION['fb_access_token'];
-		unset $_SESSION['user_name'];
-		unset $_SESSION['user_email'];
-		unset $_SESSION['user_id'];
+		// unset $_SESSION['fb_access_token'];
+		// unset $_SESSION['user_name'];
+		// unset $_SESSION['user_email'];
+		// unset $_SESSION['user_id'];
 		
 		session_destroy();
 		header('Location: '.$logouturl);
