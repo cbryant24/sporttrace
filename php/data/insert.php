4fb0f1aa45da15096bbee6f5625fad1d17a24364
Chico-Navez <ngiannulli@gmail.com>
2017-09-25 12:57:08 -0700

    finished post?

--- php/data/insert.php
+++ php/data/insert.php
@@ -1,18 +1,24 @@
 <?php
+// WRITE REGEX CHECKER HERE?
+// WRITE REGEX CHECKER HERE?
+// WRITE REGEX CHECKER HERE?
 
 
-// WRITE REGEX CHECKER HERE
-// WRITE REGEX CHECKER HERE
-// WRITE REGEX CHECKER HERE
-
-//print_r($_POST);
+if(!isset($_SESSION['fb_access_token'])){
+	echo "Not Logged In";
+	die();
+};
 
 $query = "INSERT INTO `sportsfinder-db`.`game_table` (`user_id`, `title`, `date`, `time`, `lat`, `lon`, `desc`, `address`) 
+VALUES ('{$_SESSION['user_id']}','{$_POST['game_title']}', '{$_POST['game_date']}', '{$_POST['game_time']}', '{$_POST['lat_lon']['lat']}', '{$_POST['lat_lon']['lon']}', '{$_POST['game_description']}', '{$_POST['game_address']}') 
 
-VALUES ('123','{$_POST['game_title']}', '{$_POST['game_date']}', '{$_POST['game_time']}', '{$_POST['lat_lon']['lat']}', '{$_POST['lat_lon']['lon']}', '{$_POST['game_description']}', '{$_POST['game_address']}') ";
+INSERT INTO `sportsfinder-db`.`game_history`(`user_id`, `game_id`) 
+VALUES ('{$_SESSION['user_id']}', LAST_INSERT_ID())";
 
-// VALUES ('$_SESSION[facebook_id]','$_POST[title]', '$_POST[date]', '$_POST[time]', '$_POST[lat]', '$_POST[long]', '$_POST[desc]', '$_POST[address]')
-//VALUES ('123', '4v4 Game', '2017-09-12', '23:00:00', '14.657997', '14.657997', 'lorem lorem lorem lorem lorem lorem lorem lorem', '432 Learningfuze Ave. Irvine, CA')
+
+
+//(SELECT `game_id` FROM `sportsfinder-db`.`game_table` WHERE `user_id`='{$_SESSION['user_id']}'))
+
 
 $result = mysqli_query($conn, $query);
 
