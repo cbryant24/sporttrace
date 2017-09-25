4866478cbdefee32f1fe82f015dcdccf6ecd1ca7
Chico-Navez <ngiannulli@gmail.com>
2017-09-25 14:11:27 -0700

    finished post?

--- php/sessionStart.php
+++ php/sessionStart.php
@@ -1,5 +1,5 @@
 <?php 
   session_start();//start your session
-  include('facebook-login/facebook_info.php');//make sure to include your facebook credentials!
-  include('facebook-login/libraries/php-graph-sdk/src/Facebook/autoload.php');//then you'll need to include the facebook sdk
- ?>
+  include('../facebook-login/facebook_info.php');//make sure to include your facebook credentials!
+  include('../facebook-login/libraries/php-graph-sdk/src/Facebook/autoload.php');//then you'll need to include the facebook sdk
+?>
