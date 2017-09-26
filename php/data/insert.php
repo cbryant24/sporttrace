5cf3aaa29ffdaa1128b4cd478700cef96a31a8f7
Chico-Navez <ngiannulli@gmail.com>
2017-09-25 21:35:02 -0700

    testing

--- php/data/insert.php
+++ php/data/insert.php
@@ -17,10 +17,10 @@ if($conn == false){
 print_r($_POST);
 
 $query = "INSERT INTO `sportsfinder-db`.`game_table` (`user_id`, `title`, `date`, `time`, `lat`, `lon`, `desc`, `address`, `vibe`) 
-VALUES ('{$_SESSION['user_id']}','{$_POST['complete_game']['game_title']}', '{$_POST['complete_game']['game_date']}', '{$_POST['complete_game']['game_time']}', '{$_POST['complete_game']['lat_lon']['lat']}', '{$_POST['complete_game']['lat_lon']['lon']}', '{$_POST['complete_game']['game_description']}', '{$_POST['complete_game']['game_address']}', {$_POST['complete_game']['game_vibe']}) 
+VALUES ('{$_SESSION['user_id']}','{$_POST['complete_game']['game_title']}', '{$_POST['complete_game']['game_date']}', '{$_POST['complete_game']['game_time']}', '{$_POST['complete_game']['lat_lon']['lat']}', '{$_POST['complete_game']['lat_lon']['lon']}', '{$_POST['complete_game']['game_description']}', '{$_POST['complete_game']['game_address']}', {$_POST['complete_game']['game_vibe']});
 
 INSERT INTO `sportsfinder-db`.`game_history`(`user_id`, `game_id`) 
-VALUES ('{$_SESSION['user_id']}	', LAST_INSERT_ID())";
+VALUES ('{$_SESSION['user_id']}	', LAST_INSERT_ID());";
 
 
 
