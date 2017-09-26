71db2c0d1b63081a61b5007144647ccb6d7e8eab
Chico-Navez <ngiannulli@gmail.com>
2017-09-25 17:25:55 -0700

    testing post page

--- postgame.php
+++ postgame.php
@@ -99,10 +99,10 @@
 
     </header>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyARkl4ZSeJFaXlp38h9_webHWrsOCUMSFM
-                    &libraries=places&callback=initAutocomplete"></script>
+    
     <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
-    <script type="text/javascript" src="post_game.js"></script>
+    <script type="text/javascript" src="post_game.js"></script><script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyARkl4ZSeJFaXlp38h9_webHWrsOCUMSFM
+                    &libraries=places&callback=initAutocomplete"></script>
 
 <!-- CONTENT END --><!-- CONTENT END --><!-- CONTENT END --><!-- CONTENT END -->
 
