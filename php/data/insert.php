140f7d7f866f332ead7eb4a19e95823699d33511
Chico-Navez <ngiannulli@gmail.com>
2017-09-25 21:33:43 -0700

    testing

--- php/data/insert.php
+++ php/data/insert.php
@@ -14,7 +14,7 @@ if($conn == false){
 	print_r($output['errors']);
 }
 
-print_r($_POST['complete_game']['game_title']);
+print_r($_POST);
 
 $query = "INSERT INTO `sportsfinder-db`.`game_table` (`user_id`, `title`, `date`, `time`, `lat`, `lon`, `desc`, `address`, `vibe`) 
 VALUES ('{$_SESSION['user_id']}','{$_POST['complete_game']['game_title']}', '{$_POST['complete_game']['game_date']}', '{$_POST['complete_game']['game_time']}', '{$_POST['complete_game']['lat_lon']['lat']}', '{$_POST['complete_game']['lat_lon']['lon']}', '{$_POST['complete_game']['game_description']}', '{$_POST['complete_game']['game_address']}', {$_POST['complete_game']['game_vibe']}) 
