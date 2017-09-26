2db6956b9d54e226a427786ddd1d3c25bd412f28
Chico-Navez <ngiannulli@gmail.com>
2017-09-25 20:58:24 -0700

    testing post page

--- php/data/insert.php
+++ php/data/insert.php
@@ -13,6 +13,7 @@ if($conn == false){
 	$output['errors'][] = "Bad Connection";
 	print_r($output['errors']);
 }
+
 $query = "INSERT INTO `sportsfinder-db`.`game_table` (`user_id`, `title`, `date`, `time`, `lat`, `lon`, `desc`, `address`, `vibe`) 
 VALUES ('{$_SESSION['user_id']}','{$_POST['game_title']}', '{$_POST['game_date']}', '{$_POST['game_time']}', '{$_POST['lat_lon']['lat']}', '{$_POST['lat_lon']['lon']}', '{$_POST['game_description']}', '{$_POST['game_address']}', {$_POST['game_vibe']}) 
 
@@ -36,7 +37,7 @@ if($result){
 } else {
 	$output['errors'][] = 'Database Error';
 }
-print_r($output);
+// print_r($output);
 
 		
 	
