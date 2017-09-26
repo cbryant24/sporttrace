d5edd9d2f0a7f6862606833aa597521f074d2332
Chico-Navez <ngiannulli@gmail.com>
2017-09-25 21:39:10 -0700

    testing

--- php/data/insert.php
+++ php/data/insert.php
@@ -14,7 +14,7 @@ if($conn == false){
 	print_r($output['errors']);
 }
 
-print_r($_POST);
+// print_r($_POST);
 
 $query = "INSERT INTO `sportsfinder-db`.`game_table` (`user_id`, `title`, `date`, `time`, `lat`, `lon`, `desc`, `address`, `vibe`) 
 VALUES ('{$_SESSION['user_id']}','{$_POST['complete_game']['game_title']}', '{$_POST['complete_game']['game_date']}', '{$_POST['complete_game']['game_time']}', '{$_POST['complete_game']['lat_lon']['lat']}', '{$_POST['complete_game']['lat_lon']['lon']}', '{$_POST['complete_game']['game_description']}', '{$_POST['complete_game']['game_address']}', {$_POST['complete_game']['game_vibe']});
@@ -32,7 +32,7 @@ $result = mysqli_query($conn, $query);
 if($result){
 	if (mysqli_affected_rows($conn)){
 		$output['success'] = true;
-		print_r($output['success']);
+		print_r('Good Shit');
 
 	} else {
 		$output['errors'][] = 'Insert Error';
