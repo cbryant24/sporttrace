b50d2e1a48f49d02cba601539ae3cd76220dc836
Chico-Navez <ngiannulli@gmail.com>
2017-09-28 17:46:54 -0700

    testing your games page

--- php/data/read-p.php
+++ php/data/read-p.php
@@ -5,7 +5,7 @@
 
 $query = " SELECT * FROM `game_history`
 WHERE `user_id`={$_POST['user_id']} ";
-
+print_r($_POST);
 print_r($query);
 
 die();
@@ -15,32 +15,6 @@ die();
 
 
 
-
-$result = mysqli_query($conn, $query); //send the query to the database, store the result of the query into $result
-$output = [
-	'success' => false
-];
-
-if(empty($result)){ //check if $result is empty. 
-	$output['errors'][] = 'Database Error';
-
-} else {
-	if (mysqli_num_rows($result)){ //check if any data came back
-		$output['success'] = true; //if it did, change output success to true
-		$output['data']=[];
 		
-		while( $row = mysqli_fetch_assoc($result) ){//do a while loop to collect all the data 
-			$output['data'][] = $row;//add each row of data to the $output['data'] array
-
-		}
-		print_r(json_encode($output['data']));
-		// print_r(mysqli_fetch_assoc($result));
-
-	} else {
-		$output['errors'][] = 'No Data';//if not, add to the errors: 'no data'
-
-	}
-}
-			
 	
 ?>
