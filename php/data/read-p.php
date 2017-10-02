099c46beb13510f7010c7e833810ca42087a9722
Chico-Navez <ngiannulli@gmail.com>
2017-10-02 14:11:08 -0700

    testing your games page

--- php/data/read-p.php
+++ php/data/read-p.php
@@ -31,26 +31,26 @@ $result = mysqli_query($conn, $query);  // send the query to the database, store
 
 
 
-$output = [
-	'success' => false
-];
+// $output = [
+// 	'success' => false
+// ];
 
-if(empty($result)){
-	$output['errors'][] = 'Database Error';
-} else {
-	if (mysqli_num_rows($result)){ 
-		$output['success'] = true;
-		$output['data']=[];
+// if(empty($result)){
+// 	$output['errors'][] = 'Database Error';
+// } else {
+// 	if (mysqli_num_rows($result)){ 
+// 		$output['success'] = true;
+// 		$output['data']=[];
 		
-		while( $row = mysqli_fetch_assoc($result) ){
-			$output['data'][] = $row;
-		}
-		print_r(json_encode($output['data']));
+// 		while( $row = mysqli_fetch_assoc($result) ){
+// 			$output['data'][] = $row;
+// 		}
+// 		print_r(json_encode($output['data']));
 
-	} else {
-		$output['errors'][] = 'No Data';
-	}
-}
+// 	} else {
+// 		$output['errors'][] = 'No Data';
+// 	}
+// }
 			
 
 
