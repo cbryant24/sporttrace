8b19cc7cf220ad754736c5e40093da3128ea407c
Chico-Navez <ngiannulli@gmail.com>
2017-10-02 15:13:40 -0700

    testing your games page

--- php/data/read-p.php
+++ php/data/read-p.php
@@ -2,13 +2,13 @@
 
 $query = "SELECT 
 `game_table`.`title`, `game_table`.`date`, `game_table`.`time`, `game_table`.`lat`, `game_table`.`lon`, `game_table`.`desc`,`game_table`.`address`,`game_table`.`vibe`,`game_table`.`game_id`
-FROM `game_table`
-INNER JOIN `game_history`
+FROM `sportsfinder-db`.`game_table`
+INNER JOIN `sportsfinder-db`.`game_history`
 ON `game_table`.`game_id`=`game_history`.`game_id`
 WHERE `game_history`.`user_id` =10214489178802973";
 
 
-// print_r($query);
+print_r($query);
 
 
 
@@ -16,26 +16,26 @@ $result = mysqli_query($conn, $query);  // send the query to the database, store
 
 
 
-$output = [
-	'success' => false
-];
+// $output = [
+// 	'success' => false
+// ];
 
-if(empty($result)){
-	$output['errors'][] = 'Database Error';
-} else {
-	if (mysqli_num_rows($result)){ 
-		$output['success'] = true;
-		$output['data']=[];
+// if(empty($result)){
+// 	$output['errors'][] = 'Database Error';
+// } else {
+// 	if (mysqli_num_rows($result)){ 
+// 		$output['success'] = true;
+// 		$output['data']=[];
 		
-		while( $row = mysqli_fetch_assoc($result) ){
-			$output['data'][] = $row;
-		}
-		print_r(json_encode($output['data']));
+// 		while( $row = mysqli_fetch_assoc($result) ){
+// 			$output['data'][] = $row;
+// 		}
+// 		print_r(json_encode($output['data']));
 
-	} else {
-		$output['errors'][] = 'No Data';
-	}
-}
+// 	} else {
+// 		$output['errors'][] = 'No Data';
+// 	}
+// }
 			
 
 
