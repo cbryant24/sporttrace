5091fba0ef78e4e3e98283e9b6cb273234b72c67
Chico-Navez <ngiannulli@gmail.com>
2017-10-04 18:43:31 -0700

    Reacts LAst Chance :"(

--- facebook-login/logout.php
+++ facebook-login/logout.php
@@ -2,7 +2,7 @@
 	session_start();
 
 	if(isset($_SESSION['fb_access_token'])){
-		$logouturl = 'https://www.facebook.com/logout.php?next='.'http://sporttrace.com/'.'&access_token='.$_SESSION['fb_access_token'];
+		$logouturl = 'https://www.facebook.com/logout.php?next='.'sporttrace.com/'.'&access_token='.$_SESSION['fb_access_token'];
 
 		// unset $_SESSION['fb_access_token'];
 		// unset $_SESSION['user_name'];
