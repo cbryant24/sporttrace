db535d2dbcf77f96981e4ace98bd7ff9e49697f4
cbryant24 <chrs.bryant@gmail.com>
2017-10-05 23:38:13 -0700

    map list display debugging

--- src/components/game.js
+++ src/components/game.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 import { get_single_game } from '../actions';
 
  const Game = props => {
-     console.log('these are the prop from the Game creator', props)
-     console.log('these are the prop from the Game creator', props.active_games)
+     console.log('these are the prop from the Game creator 11:37pm', props)
      
     const {desc, date, time, title, vibe} = props.game_info;
     
@@ -15,8 +14,8 @@ import { get_single_game } from '../actions';
 
     return (
         <div className='single-game row'>
-            <div className='col-3 textpad'>{game_title}</div>
-            <div className='col-3 textpad'>{`${game_date} ${game_time}`}</div> 
+            <div className='col-3 textpad'>{title}</div>
+            <div className='col-3 textpad'>{`${date} ${time}`}</div> 
             <div className='col-2 textpad'>{ vibe }</div>
             <div className='col-4'>
                 <button onClick={ () => handle_view_click() } className='btn btn-outline btn-xl viewbtn'>View</button>
