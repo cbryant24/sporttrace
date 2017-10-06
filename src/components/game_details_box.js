a62d10122db501d152c86035fa782b5108bcc5d0
cbryant24 <chrs.bryant@gmail.com>
2017-10-06 12:16:59 -0700

    debugging update game details box

--- src/components/game_details_box.js
+++ src/components/game_details_box.js
@@ -16,6 +16,7 @@ class Game_Details_Box extends Component {
     handle_join_game_click() {
         console.log('these are the details for clicking join game', this.props)        
         game_id = this.props.game_id
+        console.log(game_id)
         axios.post('php/data.php?action=join', { game_id })
         this.props.history.push('/your_games')
     }
@@ -28,12 +29,15 @@ class Game_Details_Box extends Component {
         // const {auth} = this.props;
         if(this.props.active_games.data) {
             if(this.props.active_games.data.data.length > 0) {
+                let selected_game = this.props.active_games.data.data.filter( (item) => {
+                    return item.id === this.props.active_games.game_id
+                })
                 return (
                     <div className='col-lg-4 col-12' id="game_details_box">
                     <div className='gameinfobox'>
-                        <h3>{this.props.active_games.data.data[0].title}</h3>
-                        <p>{this.props.active_games.data.data[0].desc}</p>
-                        <h6>{`${this.props.active_games.data.data[0].time} ${this.props.active_games.data.data[0].date}`}</h6>
+                        <h3>{selected_game.title}</h3>
+                        <p>{selected_game.desc}</p>
+                        <h6>{`${selected_game.time} ${selected_game.date}`}</h6>
                         <button onClick={ () => this.handle_join_game_click()} className='btn btn-outline btn-xl joinbtn'>Join Game</button>
                     </div>
                 </div>
